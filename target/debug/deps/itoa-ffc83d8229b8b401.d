8c4a3db43852cb87e7a88f30c9c0e922cffd8a3f
Fabio Durigon <develop@dury.it>
2023-01-17 18:19:04 +0100

    correct .gitignore

--- target/debug/deps/itoa-ffc83d8229b8b401.d
+++
@@ -1,6 +0,0 @@
-e:\Dev\rust\lib\dlog\target\debug\deps\itoa-ffc83d8229b8b401.rmeta: C:\Users\fabio.PHOTOREC\.cargo\registry\src\github.com-1ecc6299db9ec823\itoa-1.0.4\src\lib.rs C:\Users\fabio.PHOTOREC\.cargo\registry\src\github.com-1ecc6299db9ec823\itoa-1.0.4\src\udiv128.rs
-
-e:\Dev\rust\lib\dlog\target\debug\deps\itoa-ffc83d8229b8b401.d: C:\Users\fabio.PHOTOREC\.cargo\registry\src\github.com-1ecc6299db9ec823\itoa-1.0.4\src\lib.rs C:\Users\fabio.PHOTOREC\.cargo\registry\src\github.com-1ecc6299db9ec823\itoa-1.0.4\src\udiv128.rs
-
-C:\Users\fabio.PHOTOREC\.cargo\registry\src\github.com-1ecc6299db9ec823\itoa-1.0.4\src\lib.rs:
-C:\Users\fabio.PHOTOREC\.cargo\registry\src\github.com-1ecc6299db9ec823\itoa-1.0.4\src\udiv128.rs:
--- target/debug/deps/libc-bb5a87e981a017f2.d
+++
@@ -1,9 +0,0 @@
-e:\Dev\rust\lib\dlog\target\debug\deps\libc-bb5a87e981a017f2.rmeta: C:\Users\fabio.PHOTOREC\.cargo\registry\src\github.com-1ecc6299db9ec823\libc-0.2.138\src\lib.rs C:\Users\fabio.PHOTOREC\.cargo\registry\src\github.com-1ecc6299db9ec823\libc-0.2.138\src\macros.rs C:\Users\fabio.PHOTOREC\.cargo\registry\src\github.com-1ecc6299db9ec823\libc-0.2.138\src\fixed_width_ints.rs C:\Users\fabio.PHOTOREC\.cargo\registry\src\github.com-1ecc6299db9ec823\libc-0.2.138\src\windows\mod.rs C:\Users\fabio.PHOTOREC\.cargo\registry\src\github.com-1ecc6299db9ec823\libc-0.2.138\src\windows\msvc\mod.rs
-
-e:\Dev\rust\lib\dlog\target\debug\deps\libc-bb5a87e981a017f2.d: C:\Users\fabio.PHOTOREC\.cargo\registry\src\github.com-1ecc6299db9ec823\libc-0.2.138\src\lib.rs C:\Users\fabio.PHOTOREC\.cargo\registry\src\github.com-1ecc6299db9ec823\libc-0.2.138\src\macros.rs C:\Users\fabio.PHOTOREC\.cargo\registry\src\github.com-1ecc6299db9ec823\libc-0.2.138\src\fixed_width_ints.rs C:\Users\fabio.PHOTOREC\.cargo\registry\src\github.com-1ecc6299db9ec823\libc-0.2.138\src\windows\mod.rs C:\Users\fabio.PHOTOREC\.cargo\registry\src\github.com-1ecc6299db9ec823\libc-0.2.138\src\windows\msvc\mod.rs
-
-C:\Users\fabio.PHOTOREC\.cargo\registry\src\github.com-1ecc6299db9ec823\libc-0.2.138\src\lib.rs:
-C:\Users\fabio.PHOTOREC\.cargo\registry\src\github.com-1ecc6299db9ec823\libc-0.2.138\src\macros.rs:
-C:\Users\fabio.PHOTOREC\.cargo\registry\src\github.com-1ecc6299db9ec823\libc-0.2.138\src\fixed_width_ints.rs:
-C:\Users\fabio.PHOTOREC\.cargo\registry\src\github.com-1ecc6299db9ec823\libc-0.2.138\src\windows\mod.rs:
-C:\Users\fabio.PHOTOREC\.cargo\registry\src\github.com-1ecc6299db9ec823\libc-0.2.138\src\windows\msvc\mod.rs:
